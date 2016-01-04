6975799fbb915280ffb8d3e6872fdc07e5236243
rowtn <haha@nope.notyet.net>
2016-01-04 20:52:01 +1100

    Add Utils::combinations

--- src/main/java/io/kry/adventofenterprise/framework/Utils.java
+++ src/main/java/io/kry/adventofenterprise/framework/Utils.java
@@ -1,8 +1,12 @@
 package io.kry.adventofenterprise.framework;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Utils {
 
@@ -28,5 +32,36 @@ public class Utils {
 
             permutations(newHead, newTail, permutations);
         }
+    }
+
+    public static <T> List<List<T>> combinations(List<T> original, int k) {
+        List<List<T>> all = new ArrayList<>();
+        int r = 0, index = 0, n = original.size();
+        List<Integer> combination = new ArrayList<>(Collections.nCopies(k, 0));
+        while (r >= 0) {
+            if (index <= n + r - k) {
+                combination.set(r, index);
+                if (r == k - 1) {
+                    //all.add(original.stream().filter(t -> combination.contains(original.indexOf(t))).collect(Collectors.toList()));
+                    List<T> next = new ArrayList<>();
+                    for (Integer i : combination) {
+                        next.add(original.get(i));
+                    }
+                    all.add(next);
+                    index++;
+                } else {
+                    index = combination.get(r) + 1;
+                    r++;
+                }
+            } else {
+                r--;
+                if (r > 0) {
+                    index = combination.get(r) + 1;
+                } else {
+                    index = combination.get(0) + 1;
+                }
+            }
+        }
+        return all;
     }
 }
